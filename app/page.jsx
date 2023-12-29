47b4e9077903ea25fecc8b641db2eab6943d81ed
M Rizky Ramadhani <69834119+Mokyra18@users.noreply.github.com>
2023-12-29 14:46:57 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import TopicsList from "@/components/TopicsList";
 
 export default function Home() {
-  // return <TopicsList />;
-  return <div>hello</div>
+  return <TopicsList />;
+  // return <div>hello</div>
 }
